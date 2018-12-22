ff3ddb85d8f4cffdb6200e213d5354580539fbc8
Iqbal Syamil Ayasy <2pai@Iqbals-MacBook-Pro.local>
2018-12-22 14:09:51 +0700

    add eslint & fix code with eslint

--- bin/auth/basic_auth.js
+++ bin/auth/basic_auth.js
@@ -3,18 +3,18 @@ const { BasicStrategy } = require('passport-http');
 const User = require('./auth_user');
 
 passport.use(new BasicStrategy(
-    (username,password,callback) => {
-        User.findByUser(username,(user) => {
-            if(!user) return callback(null,false);
-            if(!user.isValidPassword(password)) return callback(null,false);
-            return callback(null,user);
-        });
-    }));
+  (username,password,callback) => {
+    User.findByUser(username,(user) => {
+      if(!user) return callback(null,false);
+      if(!user.isValidPassword(password)) return callback(null,false);
+      return callback(null,user);
+    });
+  }));
 
 const isAuth = passport.authenticate('basic',{session: false });
 const init = () => passport.initialize();
 
 module.exports = {
-    isAuth,
-    init
-}
+  isAuth,
+  init
+};
