bf5ca8ce7c37a6759da04efad8190c427bfe85f7
Iqbal Syamil Ayasy <2pai@Iqbals-MacBook-Pro.local>
2019-01-03 20:24:16 +0700

    add cluster

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-require('dotenv').config()
+require('dotenv').config();
+const cluster = require('cluster');
 const server = require('./bin/app/server');
-const logger = require('./bin/helper/util/logger')
-const AppServer = new server();
+const logger = require('./bin/helper/util/logger');
+const cpuCount = require('os').cpus().length;
 
-const Port = process.env.SERVER_PORT || 1337;
+if(cluster.isMaster){
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
 
-AppServer.server.listen(Port,(req,res) => {
+}else{
+  const AppServer = new server();
+
+  const Port = process.env.SERVER_PORT || 1337;
+
+  AppServer.server.listen(Port,() => {
     let ctx = 'app-server';
-logger.log(ctx,`The server is running on ${Port}`,'init')
+    logger.log(ctx,`The server is running on ${Port} workerid = ${cluster.worker.id}`,'init');
+  });
+}
+cluster.on('exit', (worker) => {
+  logger.log('server',`Worker %d died :( ${worker.id}`,'init');
+  cluster.fork();
+
 });
+
