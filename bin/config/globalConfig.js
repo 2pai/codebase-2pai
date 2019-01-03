76c0e843fa66489862e21f6fa3463631bcc21d78
Iqbal Syamil Ayasy <2pai@Iqbals-MacBook-Pro.local>
2019-01-03 20:26:44 +0700

    add jwt config

--- bin/config/globalConfig.js
+++ bin/config/globalConfig.js
@@ -7,10 +7,20 @@ const getEnvironmentStatus = () => process.env.ENVIRONMENT;
 const getBasicAuthUsername = () => process.env.BASIC_AUTH_USERNAME;
 const getBasicAuthPassword = () => process.env.BASIC_AUTH_PASSWORD;
 
+const getPrivateKey = () => process.env.JWT_PRIVATE_KEY;
+const getPublicKey = () => process.env.JWT_PUBLIC_KEY;
+
+const getJWTIssuer = () => process.env.JWT_ISSUER;
+const getJWTaudience = () => process.env.JWT_AUDIENCE;
+
 module.exports = {
   getDevelopmentMongoServer,
   getEnvironmentStatus,
   getBasicAuthUsername,
-  getBasicAuthPassword
+  getBasicAuthPassword,
+  getPublicKey,
+  getPrivateKey,
+  getJWTIssuer,
+  getJWTaudience
 
 };
