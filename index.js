2979652765f00d0fedf977f2a681838651874bf3
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-03-27 18:33:55 +0700

    Fix MongoDB

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cluster = require('cluster');
 const server = require('./bin/app/server');
 const logger = require('./bin/helper/util/logger');
 const cpuCount = require('os').cpus().length;
+const mongoDB = require('./bin/infra/databases/mongo/connection');
+const Port = process.env.SERVER_PORT || 1337;
+const AppServer = new server();
 
 if(cluster.isMaster){
   for (let i = 0; i < cpuCount; i += 1) {
@@ -10,11 +13,9 @@ if(cluster.isMaster){
   }
 
 }else{
-  const AppServer = new server();
-
-  const Port = process.env.SERVER_PORT || 1337;
 
   AppServer.server.listen(Port,() => {
+    mongoDB.init();
     let ctx = 'app-server';
     logger.log(ctx,`The server is running on ${Port} workerid = ${cluster.worker.id}`,'init');
   });
@@ -22,6 +23,5 @@ if(cluster.isMaster){
 cluster.on('exit', (worker) => {
   logger.log('server',`Worker %d died :( ${worker.id}`,'init');
   cluster.fork();
-
 });
 
