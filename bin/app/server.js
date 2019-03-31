705df5c907564c8e618d456192aab6c9092e8cd7
Iqbal syamil ayasy <22183588+2pai@users.noreply.github.com>
2019-03-31 16:43:21 +0700

    add Authorization in CORS

--- bin/app/server.js
+++ bin/app/server.js
@@ -9,7 +9,7 @@ const jwt = require('../auth/jwt_auth');
 const crossOrigin = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     res.send(200);
   }
